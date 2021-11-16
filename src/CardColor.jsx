b71afee709cc04028ebc0fe05c4a894e7c6c7503
evannyguedes <evannyguedes@gmail.com>
2021-11-16 13:30:34 +0100

    Changed CardColor compenent code
    Now the code is cleaner

--- src/CardColor.jsx
+++ src/CardColor.jsx
@@ -1,75 +1,29 @@
 export default function CardColor(weatherData){
-
-    //let backgroundColor=  "01d";
-    let backgroundColor=  weatherData.icon;
+  const codeMapping = {
+    "01d": "linear-gradient(180deg, rgb(162, 218, 246) 0%, rgb(195, 246, 246) 23%, rgb(250, 245, 245) 90%)",
+    "01n": "linear-gradient(172deg, rgb(27, 39, 122) 0%, rgb(235, 232, 241) 100%)",
+    "02d": "linear-gradient(178deg, rgb(141, 185, 223) 0%, rgb(187, 205, 224) 11%, rgb(245, 248, 250) 65%)",
+    "02n": "linear-gradient(180deg, rgb(20, 35, 88) 0%, rgb(132, 148, 198) 41%, rgb(225, 237, 227) 100%)",
+    "03d": "linear-gradient(178deg, rgb(141, 185, 223) 0%, rgb(187, 205, 224) 11%, rgb(245, 248, 250) 65%)",
+    "03n": "linear-gradient(180deg, rgb(20, 35, 88) 0%, rgb(132, 148, 198) 41%, rgb(225, 237, 227) 100%)",
+    "04d": "linear-gradient(178deg, rgb(141, 185, 223) 0%, rgb(187, 205, 224) 11%, rgb(245, 248, 250) 65%)",
+    "04n": "linear-gradient(180deg, rgb(20, 35, 88) 0%, rgb(132, 148, 198) 41%, rgb(225, 237, 227) 100%)",
+    "09d": "linear-gradient(178deg, rgb(210, 225, 249) 19%, rgb(200, 228, 236) 33%, rgb(245, 248, 250) 65%)",
+    "09n": "linear-gradient(178deg, rgb(210, 225, 249) 19%, rgb(200, 228, 236) 33%, rgb(245, 248, 250) 65%)",
+    "10d": "linear-gradient(178deg, rgb(177, 189, 220) 5%, rgb(216, 223, 239) 44%, rgb(245, 248, 250) 78%)",
+    "10n": "linear-gradient(178deg, rgb(177, 189, 220) 5%, rgb(216, 223, 239) 44%, rgb(245, 248, 250) 78%)",
+    "11d": "linear-gradient(161deg, rgb(29, 53, 73) 0%, rgb(200, 211, 224) 73%, rgb(245, 248, 250) 100%)",
+    "11n": "linear-gradient(161deg, rgb(29, 53, 73) 0%, rgb(200, 211, 224) 73%, rgb(245, 248, 250) 100%)",
+    "13d": "linear-gradient(178deg, rgb(222, 230, 231) 0%, rgb(231, 241, 241) 52%, rgb(245, 248, 250) 100%)",
+    "13n": "linear-gradient(178deg, rgb(222, 230, 231) 0%, rgb(231, 241, 241) 52%, rgb(245, 248, 250) 100%)",
+    "50d": "linear-gradient(178deg, rgb(183, 190, 183) 0%, rgb(209, 222, 210) 11%, rgb(245, 248, 250) 65%)",
+    "50n": "linear-gradient(178deg, rgb(183, 190, 183) 0%, rgb(209, 222, 210) 11%, rgb(245, 248, 250) 65%)",
+  };
+    
+    let backgroundColor=  codeMapping[weatherData.icon];
     let custom_style;
-    switch(backgroundColor){
-    case "01d":
-
       custom_style = {
-        background:"linear-gradient(180deg, rgb(162, 218, 246) 0%, rgb(195, 246, 246) 23%, rgb(250, 245, 245) 90%)"
+        background: backgroundColor
       }
-      return custom_style
-    case "01n":
-      custom_style = {
-        background:"linear-gradient(172deg, rgb(27, 39, 122) 0%, rgb(235, 232, 241) 100%)"
-      }
-      return custom_style
-    case "02d":
-      custom_style = {
-        background:"linear-gradient(178deg, rgb(141, 185, 223) 0%, rgb(187, 205, 224) 11%, rgb(245, 248, 250) 65%)"
-      }
-      return custom_style
-    case "03d":
-      custom_style = {
-        background:"linear-gradient(178deg, rgb(141, 185, 223) 0%, rgb(187, 205, 224) 11%, rgb(245, 248, 250) 65%)"
-      }
-      return custom_style
-    case "04d":
-      custom_style = { 
-        background:"linear-gradient(178deg, rgb(141, 185, 223) 0%, rgb(187, 205, 224) 11%, rgb(245, 248, 250) 65%)"
-      }
-      return custom_style
-    case "02n":
-      custom_style = {
-        background:"linear-gradient(180deg, rgb(20, 35, 88) 0%, rgb(132, 148, 198) 41%, rgb(225, 237, 227) 100%)"
-      }
-      return custom_style
- 
-    case "03n":
-      custom_style = { 
-        background:"linear-gradient(180deg, rgb(20, 35, 88) 0%, rgb(132, 148, 198) 41%, rgb(225, 237, 227) 100%)"
-      }
-      return custom_style
-    case "04n":
-      custom_style = { 
-        background:"linear-gradient(180deg, rgb(20, 35, 88) 0%, rgb(132, 148, 198) 41%, rgb(225, 237, 227) 100%)"
-      }
-      return custom_style
-    case "09d":
-      custom_style = {
-        background:"linear-gradient(178deg, rgb(210, 225, 249) 19%, rgb(200, 228, 236) 33%, rgb(245, 248, 250) 65%)"
-      }
-      return custom_style
-    case "10d":
-      custom_style = {
-        background:"linear-gradient(178deg, rgb(177, 189, 220) 5%, rgb(216, 223, 239) 44%, rgb(245, 248, 250) 78%)"
-      }
-      return custom_style
-    case "11d":
-      custom_style = {
-        background:"linear-gradient(161deg, rgb(29, 53, 73) 0%, rgb(200, 211, 224) 73%, rgb(245, 248, 250) 100%)"
-      }
-      return custom_style
-    case "13d":
-      custom_style = {
-        background:"linear-gradient(178deg, rgb(222, 230, 231) 0%, rgb(231, 241, 241) 52%, rgb(245, 248, 250) 100%)"
-      }
-      return custom_style
-    default:
-      custom_style = {
-        background:"linear-gradient(178deg, rgb(183, 190, 183) 0%, rgb(209, 222, 210) 11%, rgb(245, 248, 250) 65%)"
-      }
-        return custom_style 
-    }
+    return custom_style
 }
